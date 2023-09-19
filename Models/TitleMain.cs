90ed59c02f883b01a875c485dfebaf9e7a4db20e
aryantomahawk <punalekar.aryan@gmail.com>
2023-09-19 16:15:40 -0500

    added/played around with Binding operators in MAUI

---
+++ Models/TitleMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMobileApp.Models
+{
+   public static class TitleMain
+   {
+        public static String PageTitle { get; } = "Main Page View";
+        public static String StackLayoutTitle { get; } = "Stack Layout";
+    }
+}
--- ViewViewModels/Main/MainViewModel.cs
+++ ViewViewModels/Main/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MyFirstMobileApp.ViewModels;
+﻿using MyFirstMobileApp.Models;
+using MyFirstMobileApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,11 @@ namespace MyFirstMobileApp.ViewViewModels.Main
 {
     internal class MainViewModel : BaseViewModel
     {
+       public MainViewModel() {
+
+            Title = TitleMain.PageTitle;
+            StackLayoutTitle = TitleMain.StackLayoutTitle; 
+
+        }
     }
 }
