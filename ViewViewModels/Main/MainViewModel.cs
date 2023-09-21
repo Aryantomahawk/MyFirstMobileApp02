f28aa99a0cf1d9d6270e298f9acf26c568ebf6ae
aryantomahawk <punalekar.aryan@gmail.com>
2023-09-21 16:17:21 -0500

    Added Buttons

--- ViewViewModels/Main/MainViewModel.cs
+++ ViewViewModels/Main/MainViewModel.cs
@@ -5,16 +5,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MyFirstMobileApp.ViewViewModels.Main
 {
     internal class MainViewModel : BaseViewModel
     {
+
+        public String Layouts { get; set; } = TitleMain.Layouts; 
+
+        //Button Commands
+
+       public ICommand OnLayoutsClicked { get; set; }
+
        public MainViewModel() {
 
             Title = TitleMain.PageTitle;
-            StackLayoutTitle = TitleMain.StackLayoutTitle; 
+            StackLayoutTitle = TitleMain.StackLayoutTitle;
 
+            //Set Commands
+
+            OnLayoutsClicked = new Command(OnLayoutsClickedAsync);
+
+        }
+
+        private async void OnLayoutsClickedAsync()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new Layout());
         }
     }
 }
